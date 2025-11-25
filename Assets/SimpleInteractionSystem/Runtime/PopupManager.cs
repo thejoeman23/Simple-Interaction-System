e0c2641cbd1fcd384315f78c9f30ea3457fb65d7
Jonathan Strong <epicjonathan24@gmail.com>
2025-11-25 15:00:07 -0500

    Edited Popup Manager
    - Popup manager now tweens the popup in and out (without a dependency on DOTween)
    - Removed height offset variable since its on the Interactable.cs as well

--- Assets/SimpleInteractionSystem/Runtime/PopupManager.cs
+++ Assets/SimpleInteractionSystem/Runtime/PopupManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace SimpleInteractionSystem
@@ -7,77 +8,115 @@ namespace SimpleInteractionSystem
         public static PopupManager Instance;
 
         [Header("Interact Popup Settings")]
-        [SerializeField] float _interactButtonSize = 0.5f;
-        [SerializeField] private float _heightOffset = 2;
         [SerializeField] private GameObject _interactButtonPrefab;
         private GameObject _currentInteractButton;
 
+        [Header("Animation Settings")]
+        [SerializeField] private float _animationDuration = .15f;
+        [SerializeField] private AnimationCurve _animationCurve = AnimationCurve.EaseInOut(0,0,1,1);
+
+        private Vector3 _originalButtonScale;
+
         private void Awake()
         {
+            // Singleton pattern
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
             Instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+            _originalButtonScale = _interactButtonPrefab.transform.localScale;
         }
 
         public void ChangeInteractableObject(Interactable interactable)
         {
-            if (_currentInteractButton == null)
-            {
-                CreateFirstInteractButton(interactable);
-            }
-            
-            // If there is nothing to interact with then clear the interact button
+            // If there is nothing to interact with then animate out/clear the interact button
             if (interactable == null)
             {
-                ClearInteractableButton();
+                AnimateOut();
                 return;
             }
 
-            GameObject oldButton = _currentInteractButton;
+            // If there's an old button, destroy it
+            if (_currentInteractButton != null)
+            {
+                Destroy(_currentInteractButton);
+            }
+
+            // Create and activate the new button
             _currentInteractButton = CreateInteractButton(interactable);
-            
-            /*// Kill the old tween and create a new one
-            _currentTween.Kill();
-            _currentTween = DOTween.Sequence();
+            _currentInteractButton.SetActive(true);
 
-            // Tween away the old button then tween in the new one
-            _currentTween.Append(oldButton.transform.DOScale(Vector3.zero, _tweenDuration)
-                .SetEase(Ease.Linear)
-                .OnComplete(() => Destroy(oldButton)));
-            _currentTween.Append(_currentInteractButton.transform.DOScale(Vector3.one * _interactButtonSize, _tweenDuration)
-                .SetEase(Ease.Linear));
-
-            _currentTween.Play();*/
+            // Animate in
+            AnimateIn();
         }
 
-        private GameObject CreateInteractButton(Interactable interactable)
+        protected virtual GameObject CreateInteractButton(Interactable interactable)
         {
             GameObject button = Instantiate(_interactButtonPrefab, transform.parent);
             button.transform.position = interactable.transform.position + new Vector3(0, interactable.InteractPromptHeight + _heightOffset, 0);
-            button.transform.localScale = Vector3.zero;
-            
+            button.SetActive(false); // will be enabled by ChangeInteractableObject
+            button.transform.localScale = Vector3.zero; // start hidden if animating
             return button;
+        }
+
+        protected virtual void AnimateIn()
+        {
+            if (_currentInteractButton != null)
+                StartCoroutine(AnimateInCoroutine());
+        }
+
+        private IEnumerator AnimateInCoroutine()
+        {
+            _currentInteractButton.transform.localScale = Vector3.zero;
+            float timeElapsed = 0f;
+            while (timeElapsed < _animationDuration)
+            {
+                timeElapsed += Time.deltaTime;
+                float t = Mathf.Clamp01(timeElapsed / _animationDuration);
+                float scaleT = _animationCurve.Evaluate(t);
+                _currentInteractButton.transform.localScale = Vector3.Lerp(Vector3.zero, _originalButtonScale, scaleT);
+                yield return null;
+            }
+            // Ensure final value
+            _currentInteractButton.transform.localScale = _originalButtonScale;
+        }
+
+        protected virtual void AnimateOut()
+        {
+            if (_currentInteractButton != null)
+                StartCoroutine(AnimateOutCoroutine());
+        }
+
+        private IEnumerator AnimateOutCoroutine()
+        {
+            Vector3 startScale = _currentInteractButton.transform.localScale;
+            float timeElapsed = 0f;
+            while (timeElapsed < _animationDuration)
+            {
+                timeElapsed += Time.deltaTime;
+                float t = Mathf.Clamp01(timeElapsed / _animationDuration);
+                float scaleT = _animationCurve.Evaluate(1f - t); // reverse curve
+                _currentInteractButton.transform.localScale = Vector3.Lerp(Vector3.zero, startScale, scaleT);
+                yield return null;
+            }
+            // Ensure scale is zero and destroy
+            _currentInteractButton.transform.localScale = Vector3.zero;
+            Destroy(_currentInteractButton);
+            _currentInteractButton = null;
         }
 
         private void ClearInteractableButton()
         {
-            // _currentTween.Kill();
-            // _currentTween = DOTween.Sequence();
-            //
-            // _currentTween.Append(_currentInteractButton.transform.DOScale(Vector3.zero, _tweenDuration)
-            //     .SetEase(Ease.Linear)
-            //     .OnComplete(() => Destroy(_currentInteractButton)));
-            //
-            // _currentTween.Play();
-        }
-
-        private void CreateFirstInteractButton(Interactable interactable)
-        {
-            _currentInteractButton = CreateInteractButton(interactable);
-            
-            // _currentTween.Kill();
-            // _currentTween = DOTween.Sequence();
-            //
-            // _currentTween.Append(_currentInteractButton.transform.DOScale(Vector3.one * _interactButtonSize, _tweenDuration)
-            //     .SetEase(Ease.Linear));
+            // This method is now only needed if you want instant destruction w/o animation
+            if (_currentInteractButton != null)
+            {
+                Destroy(_currentInteractButton);
+                _currentInteractButton = null;
+            }
         }
     }
 }
